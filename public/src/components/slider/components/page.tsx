f976fd502a271e0445dd306f199954ee49af9ced
savelasquezo <savelasquezo@gmail.com>
2024-07-30 15:01:47 -0500

    Pajoytours V1.1

--- public/src/components/slider/components/page.tsx
+++ public/src/components/slider/components/page.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, Suspense } from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { NextResponse } from 'next/server';
 import { Carousel } from 'flowbite-react';
@@ -47,17 +47,15 @@ const Slider: React.FC<SessionInfo> = ({ session }) => {
     <div className="w-screen h-auto mx-0">
       <div id="default-carousel" className="relative" data-carousel="slide">
           <div className={`overflow-hidden relative z-0 ${session ? 'h-44 md:h-80 lg:h-[calc(100vh-112px)]' : 'h-44 md:h-80 lg:h-[calc(100vh-56px)]'}`}>
-          <Suspense fallback={null}>
-            {imagenSliders?.length > 0 ? (
-              <Carousel slide={true} slideInterval={3000} indicators={false}
-                rightControl={<svg className="w-5 h-5 text-gray-400 sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7"></path></svg>}
-                leftControl={<svg className="w-5 h-5 text-gray-400 sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7"></path></svg>}>
-                {imagenSliders?.map((imagenSlider, i) => (
-                  <Image key={i} width={1340} height={500} src={imagenSlider?.file} loader={imageLoader} priority={true} className="block w-full h-full" alt=""/>
-                ))}
-              </Carousel>
-              ) : null}
-          </Suspense>
+          {imagenSliders?.length > 0 ? (
+            <Carousel slide={true} slideInterval={3000} indicators={false}
+              rightControl={<svg className="w-5 h-5 text-gray-400 sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7"></path></svg>}
+              leftControl={<svg className="w-5 h-5 text-gray-400 sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7"></path></svg>}>
+              {imagenSliders?.map((imagenSlider, i) => (
+                <Image key={i} width={1340} height={500} src={imagenSlider?.file} loader={imageLoader} priority={true} className="block w-full h-full" alt=""/>
+              ))}
+            </Carousel>
+            ) : null}
           </div>
       </div>
     </div>
