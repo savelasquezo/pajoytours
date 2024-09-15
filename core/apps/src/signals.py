f690a02339214aa897d87183d32d0d87be4e048c
savelasquezo <savelasquezo@gmail.com>
2024-09-15 18:43:43 -0500

    Edit SignalInvoices

--- core/apps/src/signals.py
+++ core/apps/src/signals.py
@@ -1,1 +1,21 @@
+import logging
 import apps.src.models as model
+from django.db.models.signals import post_save, pre_save
+from django.dispatch import receiver
+
+logger = logging.getLogger(__name__)
+
+@receiver(pre_save, sender=model.Invoices)
+def updateInvoices(sender, instance, **kwargs):
+    obj = model.Invoices.objects.filter(pk=instance.pk).first()
+    if obj and not kwargs.get('created', False):
+        if obj.state != "done" and instance.state == "done":
+            print("Actualizo")
+            try:
+                user = instance.account
+                amount = instance.amount
+                user.balance += amount
+                user.save()
+                
+            except Exception as e:
+                logger.error("%s", e, exc_info=True)
